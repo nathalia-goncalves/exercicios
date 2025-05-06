09e264bd1aa950d8b9780231c1dee240d7f43e6f
nathalia-goncalves <00001125017740sp@al.sp.gov.br>
2025-05-06 12:11:21 -0300

    exercicio05.py

--- exercicio05.py
+++ exercicio05.py
@@ -1,14 +1,15 @@
-def cadastrar_aluno(nome,email,serie):
+def cadastrar_aluno(nome,email,serie,nota01,nota02,nota03):
     alunos = []
 
     aluno = {
         "nome": nome,
         "email": email,
-        "serie": serie
+        "serie": serie,
+        "notas": [nota01,nota02,nota03]
     }
 
     alunos.append(aluno)
 
     return alunos
 
-print(cadastrar_aluno("Nathalia", "abc@abc.com", "serie"))
+print(cadastrar_aluno("Nathalia", "abc@abc.com", "serie 2B" 8,7,9))
