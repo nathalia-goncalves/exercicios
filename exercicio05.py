e7dde69e1067828fb367efde093cabcf88827cec
nathalia-goncalves <00001125017740sp@al.sp.gov.br>
2025-05-13 07:23:04 -0300

    exercicio06.py

--- exercicio05.py
+++ exercicio05.py
@@ -1,3 +1,5 @@
+from exercicio04 import calcular_media
+
 def cadastrar_aluno(nome,email,serie,nota01,nota02,nota03):
     alunos = []
 
@@ -9,7 +11,7 @@ def cadastrar_aluno(nome,email,serie,nota01,nota02,nota03):
     }
 
     alunos.append(aluno)
-
+    media = alunos(aluno["nota"])
     return alunos
 
 print(cadastrar_aluno("Nathalia", "abc@abc.com", "serie 2B", 8,7,9))
